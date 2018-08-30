074298de05cabcc44f4fc1ada2b875fdda0ea8a6
JLashmar <jordan.lashmar@outlook.com>
2018-08-30 22:49:04 +0100

    api set up

--- England Volleyball/England_Volleyball/settings.py
+++ England Volleyball/England_Volleyball/settings.py
@@ -42,16 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #framework stuff
     'rest_framework',
-    'rest_framework_docs',
+    'coreapi',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -127,3 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- England Volleyball/England_Volleyball/urls.py
+++ England Volleyball/England_Volleyball/urls.py
@@ -4,47 +4,27 @@ Definition of urls for England_Volleyball.
 
 from datetime import datetime
 from django.conf.urls import url
-import django.contrib.auth.views
 
-import app.forms
-import app.views
+from django.urls import include, path
+from django.contrib import admin
+admin.autodiscover()
 
-# Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
-# admin.autodiscover()
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', app.views.home, name='home'),
-    url(r'^contact$', app.views.contact, name='contact'),
-    url(r'^about', app.views.about, name='about'),
-    url(r'^login/$',
-        django.contrib.auth.views.login,
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': app.forms.BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title': 'Log in',
-                'year': datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        django.contrib.auth.views.logout,
-        {
-            'next_page': '/',
-        },
-        name='logout'),
-
-    #api additions
-    url(r'^api-auth/', include('rest_framework.urls'))
-    url(r'^docs/', include('rest_framework_docs.urls')),
+    path('admin/', admin.site.urls),
+    # api additions
+    path('api-auth/', include('rest_framework.urls')),
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title='My API title',
+                                    authentication_classes=[],
+                                    permission_classes=[]))
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
 ]
